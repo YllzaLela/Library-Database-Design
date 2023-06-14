655b3d311438b1d94a9960621654f74c0928390e
YllzaLela <86922461+YllzaLela@users.noreply.github.com>
2023-06-14 15:23:01 +0200

    Add files via upload

---
+++ Tables/table_insertion.sql
@@ -0,0 +1,143 @@
+--User finished
+--INSERT INTO [user]
+--VALUES  (4907, 'Yllza' , 'Lela', 'yllza.lela@gmail.com','+38971123456', 'example road, nr 30, Struga',1);
+
+--INSERT INTO [user]
+--VALUES  (1234, 'Stu1' , 'Surname', 'student.one@gmail.com','+38971323456', 'example road, nr 56, Skopje',1);
+
+--INSERT INTO [user]
+--VALUES  (1235, 'Stu2' , 'Surname2', 'student.two@gmail.com','+38970123456', 'example2 road, nr 31, Skopje',0);
+
+--INSERT INTO [user]
+--VALUES  (1236, 'Stu3' , 'Surname3', 'student.three@gmail.com','+38971155456', 'example3 road, nr 12, Bitola',1);
+
+--INSERT INTO [user]
+--VALUES  (1238, 'Stu4' , 'Surname4', 'student.four@gmail.com','+38971123123', 'example4 road, nr 23, Ohrid',0);
+
+--Order 
+--INSERT INTO [order]
+--VALUES (1, 4907, 10-05-2022 , 10-07-2022, 'online' , 1);
+
+--INSERT INTO [order]
+--VALUES (2, 1234, 10-06-2022 , 10-08-2022, 'online' , 2);
+
+--INSERT INTO [order]
+--VALUES (3, 1235, 01-03-2022 , 01-05-2022, 'online' , 4);
+
+--INSERT INTO [order]
+--VALUES (4, 1236, 10-05-2022 , 10-07-2022, 'online' ,5);
+
+--INSERT INTO [order]
+--VALUES (5, 4907, 10-05-2022 , 10-07-2022, 'online' , 6);
+
+
+--Authors finished
+--INSERT INTO [author]
+--VALUES (12, 'Fyodor', 'Dostoyevski') ;
+
+--INSERT INTO [author]
+--VALUES (13, 'Ernest', 'Hemingway') ;
+
+--INSERT INTO [author]
+--VALUES (11, 'Mark', 'Twain') ;
+
+--INSERT INTO [author]
+--VALUES (10, 'Stephen', 'King') ;
+
+--INSERT INTO [author]
+--VALUES (14, 'Charles', 'Dickens') ;
+
+
+---------------------------------------------------------
+--Categories Finished
+---------------------------------------------------------
+--INSERT INTO [category]
+--VALUES (1, 'Computer Science' );
+
+--INSERT INTO [category]
+--VALUES (2, 'Architecture' );
+
+--INSERT INTO [category]
+--VALUES (3, 'Law' );
+
+--INSERT INTO [category]
+--VALUES (4, 'Political Science' );
+
+--INSERT INTO [category]
+--VALUES (5, 'Psychology' );
+
+--INSERT INTO [category]
+--VALUES (6, 'Business' );
+
+--INSERT INTO [category]
+--VALUES (7, 'Foreign Languages' );
+
+
+--Book 
+--INSERT INTO [book]
+--VALUES (1, 'Fundamentalas of Programming', 20,1, 12  );
+--INSERT INTO [book]
+--VALUES (2, 'Object Oriented Programming', 13 , 1 , 10  );
+--INSERT INTO [book]
+--VALUES (3, 'Psycology Book', 27 ,5, 11  );
+--INSERT INTO [book]
+--VALUES (4, 'Business book', 15 ,6, 13  );
+--INSERT INTO [book]
+--VALUES (5, 'Archi', 31 , 2, 14 );
+--INSERT INTO [book]
+--VALUES (6, 'Italian', 12, 7, 10);
+--INSERT INTO [book]
+--VALUES (7, 'Macedonian Laws', 24, 3, 12 );
+--INSERT INTO [book]
+--VALUES (8, 'Europe Politics', 19, 4, 10);
+
+--Admin
+
+----INSERT INTO [admin]
+----VALUES (1111, 'Admin', 'One', 'admin.one@gmail.com', '+389071234333', 1);
+
+----INSERT INTO [admin]
+----VALUES (2222, 'Admin2', 'Two', 'admin2.two@gmail.com', '+389070111333', 1);
+
+----INSERT INTO [admin]
+----VALUES (3333, 'Admin3', 'Three', 'admin3.three@gmail.com', '+389071987654', 0);
+
+
+--Fine
+--INSERT INTO [fine]
+--VALUES (01, 1234 , 2, 2, 10-09-2022 , 50.20);
+
+--INSERT INTO [fine]
+--VALUES (02, 4907 , 1, 1, 11-7-2022 , 50.20);
+
+
+--Report
+--INSERT INTO [report]
+--VALUES (010, 4907,  1,  10-05-2022, 02, 1111 );
+
+--INSERT INTO [report]
+--VALUES (020, 4907,  1,  10-09-2022, 02, 2222);
+
+
+
+--SELECT *
+--FROM [user];
+--SELECT *
+--FROM [book];
+--SELECT *
+--FROM [order];
+--SELECT *
+--FROM [admin];
+--SELECT *
+--FROM [author];
+--SELECT *
+--FROM [category];
+--SELECT *
+--FROM [report];
+--SELECT *
+--FROM [fine];
+
+
+
+
+
